9edfcebe33cb61dc1a7afc0a8829944f8fcacde0
Thanh Duong <dmt2426@gmail.com>
2018-09-19 22:25:27 +0700

    finish bug case

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Product;
+use App\Entity\Admin;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -19,6 +20,15 @@ class AppFixtures extends Fixture
             $product->setEan($i+10000000);
             $product->setFeatures(array("feature " . $i, "feature s" . $i));
             $manager->persist($product);
+
+
+            $admin = new Admin();
+            $admin->setUsername("test " . $i);
+            $admin->setEmail("test" . $i . "@gmail.com");
+            $admin->setEnabled(1);
+            $admin->setPassword(password_hash('123', PASSWORD_BCRYPT));
+            $admin->setRoles(['ROLE_ADMIN']); //important for login
+            $manager->persist($admin);
         }
 
         $manager->flush();
