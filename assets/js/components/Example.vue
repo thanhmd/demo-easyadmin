2d090eade5afd254979863c1b6d94fd2ce6f4179
Thanh Duong Minh <dmthanh@tma.com.vn>
2018-09-28 17:46:27 +0700

    fix bug overide template

--- assets/js/components/Example.vue
+++ assets/js/components/Example.vue
@@ -1,7 +1,16 @@
 <template>
-    <div>
-        <p>This is an example of a new components in VueJs</p>
-    </div>
+    <v-form>
+        <v-container>
+            <v-layout row wrap>
+                <v-flex xs12 sm6 md3>
+                    <v-text-field
+                            label="Regular"
+                            single-line
+                    ></v-text-field>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </v-form>
 </template>
 
 <script>
